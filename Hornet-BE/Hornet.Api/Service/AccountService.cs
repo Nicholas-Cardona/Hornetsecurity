78bfc7232a1815d4c69197a79b195f94141cf3d2
Nicholas Cardona <nicholascardona7@gmail.com>
2026-06-10 17:47:56 +0200

    Store hashed password on the db.

--- Hornet-BE/Hornet.Api/Service/AccountService.cs
+++ Hornet-BE/Hornet.Api/Service/AccountService.cs
@@ -3,6 +3,7 @@ using Hornet.Data;
 using Hornet.Data.Entities;
 using Hornet.Data.Mappers;
 using Hornet.Domain.DTOs.Account;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,7 @@ namespace Hornet.Api.Service;
 public class AccountService : IAccountService
 {
     private readonly AppDbContext _context;
+    private readonly PasswordHasher<UserEntity> passwordHasher = new();
 
     public AccountService(AppDbContext context)
     {
@@ -22,6 +24,10 @@ public class AccountService : IAccountService
         if (request.Password != request.PasswordConfirm) throw new ArgumentException("Mismatch between Password and Confirm Password");
 
         UserEntity user = UserMapper.FromRequest(request);
+
+        string hashedPassword = passwordHasher.HashPassword(user, user.Password);
+
+        user.Password = hashedPassword;
 
         var entry = await _context.Users.AddAsync(user);
 
@@ -36,7 +42,9 @@ public class AccountService : IAccountService
 
         if (dbUser == null) throw new KeyNotFoundException("User with the given email was not found");
 
-        if (dbUser.Password != request.Password) throw new InvalidCredentialException("Invalid Password");
+        var result = passwordHasher.VerifyHashedPassword(dbUser, dbUser.Password, request.Password);
+
+        if (result != PasswordVerificationResult.Success) throw new InvalidCredentialException("Invalid Password");
 
         return dbUser;
     }
