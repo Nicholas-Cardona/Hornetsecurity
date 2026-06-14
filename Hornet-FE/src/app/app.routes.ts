8121653e9733a22bbcbd9de7cb28be091ba68ca5
Nicholas Cardona <nicholascardona7@gmail.com>
2026-06-14 11:44:37 +0200

    Route to /dash on first visit

--- Hornet-FE/src/app/app.routes.ts
+++ Hornet-FE/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { SignIn } from '@pages/sign-in/sign-in';
 import { SignUp } from '@pages/sign-up/sign-up';
-import { Home } from '@pages/home/home';
 import { Dash } from '@pages/dash/dash';
 
 export const routes: Routes = [
@@ -15,7 +14,8 @@ export const routes: Routes = [
   },
   {
     path: '',
-    component: Home,
+    pathMatch: 'full',
+    redirectTo: '/dash'
   },
   {
     path: "dash",
