75d6d5b1cd1fe2bcc8ff6f6ab5668010a51ef78e
Nicholas Cardona <nicholascardona7@gmail.com>
2026-06-14 14:43:19 +0200

    Containerize backend. Still have to make sure that the database is migrated before running worker for the first time.

--- Hornet-BE/Hornet.Api/Program.cs
+++ Hornet-BE/Hornet.Api/Program.cs
@@ -74,8 +74,14 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.SlidingExpiration = true;
 });
 
+
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
